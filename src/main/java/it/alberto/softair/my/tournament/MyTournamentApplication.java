33f49ce9ea1ddc8c691307b12748fb102014e196
alberto.tramis <albertotramis@gmail.com>
2025-04-10 00:17:50 +0200

    Push iniziale

---
+++ src/main/java/it/alberto/softair/my/tournament/MyTournamentApplication.java
@@ -0,0 +1,13 @@
+package it.alberto.softair.my.tournament;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyTournamentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyTournamentApplication.class, args);
+	}
+
+}
---
+++ src/main/java/it/alberto/softair/my/tournament/ServletInitializer.java
@@ -0,0 +1,13 @@
+package it.alberto.softair.my.tournament;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(MyTournamentApplication.class);
+	}
+
+}
